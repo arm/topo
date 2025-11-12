3dd81887305349d0b09a8d7d3922bd5448be3dfd
Bartek Mucha <muchzill4@gmail.com>
2025-11-12 10:29:18 +0000

    Remove docker specifics from reusable host (#71)

--- internal/deploy/host/host.go
+++ internal/deploy/host/host.go
@@ -7,10 +7,3 @@ const Local = Host("")
 func New(targetHost string) Host {
 	return Host(targetHost)
 }
-
-func (h Host) DockerCommandArgs() []string {
-	if h == "" {
-		return nil
-	}
-	return []string{"-H", string(h)}
-}
