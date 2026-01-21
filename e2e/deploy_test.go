4cb946115672418ab4750879547f5bef9d420d75
Luke Parkin <65398091+Luke-Parkin@users.noreply.github.com>
2026-01-21 14:50:35 +0000

    Extend e2e tests (#227)
    * Move tests to deploy_test.go
    
    Signed-off-by: luke <luke.parkin@arm.com>
    
    * Clone into cwd
    
    Signed-off-by: luke <luke.parkin@arm.com>
    
    * Topo clone doens't support .
    
    Signed-off-by: luke <luke.parkin@arm.com>
    
    * Use correct dirs
    
    Signed-off-by: luke <luke.parkin@arm.com>
    
    * ci: check for race conditions
    
    * Revert "ci: check for race conditions"
    
    This reverts commit 600ef084ac1241b43be59855a495d0bc68da3f27.
    
    ---------
    
    Signed-off-by: luke <luke.parkin@arm.com>
    Co-authored-by: awphi <26072111+awphi@users.noreply.github.com>

---
+++ e2e/deploy_test.go
@@ -0,0 +1,132 @@
+package e2e
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/arm-debug/topo-cli/internal/testutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeploy(t *testing.T) {
+	testutil.RequireDocker(t)
+	vm := testutil.StartDockerVM(t)
+	topo := buildBinary(t)
+
+	t.Run("Init, add and Deploy", func(t *testing.T) {
+		projectDir := t.TempDir()
+		composeFile := filepath.Join(projectDir, "compose.yaml")
+		t.Cleanup(func() {
+			composeDown(t, composeFile, vm.SSHConnectionString)
+		})
+
+		requireInit(t, topo, projectDir)
+		require.FileExists(t, composeFile)
+		nameArgValue := "Topo"
+		expectedResponse := fmt.Sprintf("Hello %s\n", nameArgValue)
+
+		requireExtend(t, topo, projectDir, composeFile, nameArgValue)
+
+		requireDeploy(t, topo, projectDir, vm.SSHConnectionString)
+		assertResponseBody(t, "http://localhost:8080/", expectedResponse)
+	})
+
+	t.Run("Clone and deploy", func(t *testing.T) {
+		baseDir := t.TempDir()
+		cloneDir := filepath.Join(baseDir, "project")
+		composeFile := filepath.Join(cloneDir, "compose.yaml")
+		t.Cleanup(func() {
+			composeDown(t, composeFile, vm.SSHConnectionString)
+		})
+
+		nameArgValue := "Topo"
+		requireClone(t, topo, baseDir, cloneDir, "testdata/services/hello-server", fmt.Sprintf("NAME=%s", nameArgValue))
+		requireDeploy(t, topo, cloneDir, vm.SSHConnectionString)
+		expectedResponse := fmt.Sprintf("Hello %s\n", nameArgValue)
+
+		assertResponseBody(t, "http://localhost:8080/", expectedResponse)
+	})
+}
+
+func requireClone(t *testing.T, topo string, projectDir string, cloneDir string, remoteDir string, extraArgs ...string) {
+	remoteDirPath, err := filepath.Abs(remoteDir)
+	require.NoError(t, err)
+	cloneDirPath, err := filepath.Abs(cloneDir)
+	require.NoError(t, err)
+
+	args := []string{"clone", cloneDirPath, fmt.Sprintf("dir:%s", remoteDirPath)}
+	args = append(args, extraArgs...)
+	cloneCmd := exec.Command(topo, args...)
+	cloneCmd.Dir = projectDir
+	out, err := cloneCmd.CombinedOutput()
+
+	require.NoErrorf(t, err, "clone failed: %s", out)
+}
+
+func requireInit(t *testing.T, topo, projectDir string) {
+	initCmd := exec.Command(topo, "init")
+	initCmd.Dir = projectDir
+
+	out, err := initCmd.CombinedOutput()
+
+	require.NoErrorf(t, err, "init failed: %s", out)
+}
+
+func requireExtend(t *testing.T, topo, projectDir, composeFile, customName string) {
+	templateDir, err := filepath.Abs("testdata/services/hello-server")
+	require.NoError(t, err)
+	extendCmd := exec.Command(topo, "extend", composeFile,
+		fmt.Sprintf("dir:%s", templateDir), "--no-prompt", "--",
+		fmt.Sprintf("NAME=%s", customName))
+	extendCmd.Dir = projectDir
+
+	out, err := extendCmd.CombinedOutput()
+
+	require.NoErrorf(t, err, "extend failed: %s", out)
+}
+
+func requireDeploy(t *testing.T, topo, projectDir, sshTarget string, extraArgs ...string) {
+	args := []string{"deploy", "--target", sshTarget}
+	args = append(args, extraArgs...)
+
+	deployCmd := exec.Command(topo, args...)
+	deployCmd.Dir = projectDir
+
+	out, err := deployCmd.CombinedOutput()
+
+	require.NoErrorf(t, err, "deploy failed: %s", out)
+}
+
+func assertResponseBody(t *testing.T, url, wantBody string) {
+	var resp *http.Response
+	require.Eventually(t, func() bool {
+		var err error
+		resp, err = http.Get(url)
+		if err != nil {
+			return false
+		}
+		if resp.StatusCode != 200 {
+			_ = resp.Body.Close()
+			return false
+		}
+		return true
+	}, 30*time.Second, 1*time.Second, "service did not become healthy")
+	defer resp.Body.Close() //nolint:errcheck
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	assert.Equal(t, wantBody, string(body))
+}
+
+func composeDown(t *testing.T, composeFile, sshTarget string) {
+	t.Helper()
+	cmd := exec.Command("docker", "-H", "ssh://"+sshTarget, "compose", "-f", composeFile, "down", "-v")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		t.Logf("compose down failed: %v, output: %s", err, out)
+	}
+}
