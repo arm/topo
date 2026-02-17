dd98f62db9423115adce549a9389e0ef7c29ba95
logan-scully-arm <logan.scully@arm.com>
2026-02-17 15:36:28 +0000

    no more bugs in panic
    Signed-off-by: logan-scully-arm <logan.scully@arm.com>

--- cmd/topo/deploy.go
+++ cmd/topo/deploy.go
@@ -46,7 +46,7 @@ Use --dry-run to see what commands would be executed without actually running th
 
 		dryRun, err := cmd.Flags().GetBool("dry-run")
 		if err != nil {
-			panic(fmt.Sprintf("bug: dry-run flag not registered: %v", err))
+			panic(fmt.Sprintf("internal error: dry-run flag not registered: %v", err))
 		}
 
 		outputFormat, err := resolveOutput(cmd)
