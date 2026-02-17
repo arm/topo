eaaa40b187a56386c8444d78864b01882a598c54
logan-scully-arm <logan.scully@arm.com>
2026-02-17 10:23:39 +0000

    Reintroduce resolveOutput panic
    Signed-off-by: logan-scully-arm <logan.scully@arm.com>

--- cmd/topo/root.go
+++ cmd/topo/root.go
@@ -60,7 +60,7 @@ func requireTarget(cmd *cobra.Command) (string, error) {
 func resolveOutput(cmd *cobra.Command) (term.Format, error) {
 	flagValue, err := cmd.Flags().GetString("output")
 	if err != nil {
-		return term.Plain, nil
+		panic(fmt.Sprintf("bug: output flag not registered: %v", err))
 	}
 
 	v := strings.TrimSpace(strings.ToLower(flagValue))
