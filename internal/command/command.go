14e7fa6380baa1a79516010076bff667b263e89a
Bartek Mucha <bartosz.mucha@arm.com>
2026-03-03 15:30:11 +0000

    Remove #nosec 204, which are excluded via config
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/command/command.go
+++ internal/command/command.go
@@ -9,20 +9,17 @@ import (
 
 func Docker(h ssh.Host, args ...string) *exec.Cmd {
 	cmdArgs := append(hostToArgs(h), args...)
-	// #nosec G204 -- Callers validate the command args
 	return exec.Command("docker", cmdArgs...)
 }
 
 func DockerCompose(h ssh.Host, composeFile string, args ...string) *exec.Cmd {
 	composeArgs := append([]string{"compose", "-f", composeFile}, args...)
 	cmdArgs := append(hostToArgs(h), composeArgs...)
-	// #nosec G204 -- Callers validate the command args
 	return exec.Command("docker", cmdArgs...)
 }
 
 func SSHKeyGen(keyType string, keyPath string, targetHost string) *exec.Cmd {
 	sshKeyGenArgs := []string{"-t", keyType, "-f", keyPath, "-C", targetHost}
-	// #nosec G204 -- Args are controlled by topo
 	return exec.Command("ssh-keygen", sshKeyGenArgs...)
 }
 
