574790fa9fee10788a355c141c559ae5ecbd6e50
Bartek Mucha <bartosz.mucha@arm.com>
2026-03-05 14:31:42 +0000

    Remove delays
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/output/term/spinner.go
+++ internal/output/term/spinner.go
@@ -27,14 +27,15 @@ func StartSpinner(w io.Writer, message string) *Spinner {
 	frames := []string{"◜", "◝", "◞", "◟"}
 	go func() {
 		defer close(s.done)
+		ticker := time.NewTicker(100 * time.Millisecond)
+		defer ticker.Stop()
 		for i := 0; ; i++ {
+			_, _ = fmt.Fprintf(w, "\r%s %s", frames[i%len(frames)], message)
 			select {
 			case <-s.stop:
 				_, _ = fmt.Fprintf(w, "\r\033[K")
 				return
-			default:
-				_, _ = fmt.Fprintf(w, "\r%s %s", frames[i%len(frames)], message)
-				time.Sleep(100 * time.Millisecond)
+			case <-ticker.C:
 			}
 		}
 	}()
