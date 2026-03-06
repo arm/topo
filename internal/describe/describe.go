d6ddc0e6345a6ca796f267449cd05d6f2ac6e016
Tomás González <tomasagustin.gonzalezorlando@arm.com>
2026-03-06 16:14:53 +0000

    roll back for unmarshalling
    Signed-off-by: Tomás González <tomasagustin.gonzalezorlando@arm.com>

--- internal/describe/describe.go
+++ internal/describe/describe.go
@@ -41,9 +41,9 @@ func ReadTargetDescriptionFromFile(filePath string) (*target.HardwareProfile, er
 		return nil, fmt.Errorf("failed to read target description file %q: %w", filePath, err)
 	}
 
-	var profile *target.HardwareProfile
-	if err := yaml.Unmarshal(description, profile); err != nil {
+	var profile target.HardwareProfile
+	if err := yaml.Unmarshal(description, &profile); err != nil {
 		return nil, fmt.Errorf("failed to parse target description file %q: %w", filePath, err)
 	}
-	return profile, nil
+	return &profile, nil
 }
