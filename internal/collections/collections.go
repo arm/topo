8332900d5775c17272bcb726bc7c45f429047230
Bartek Mucha <bartosz.mucha@arm.com>
2026-03-09 09:08:48 +0000

    Add `collections.GroupBy`
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

---
+++ internal/collections/collections.go
@@ -0,0 +1,36 @@
+package collections
+
+type Group[I any, K comparable] struct {
+	Key     K
+	Members []I
+}
+
+func GroupBy[I any, K comparable](items []I, keyFn func(I) K) []Group[I, K] {
+	order, groupMap := groupBy(items, keyFn)
+
+	groups := make([]Group[I, K], len(order))
+
+	for i, key := range order {
+		groups[i] = Group[I, K]{
+			Key:     key,
+			Members: groupMap[key],
+		}
+	}
+
+	return groups
+}
+
+func groupBy[I any, K comparable](slice []I, keyFn func(I) K) ([]K, map[K][]I) {
+	order := []K{}
+	groups := map[K][]I{}
+
+	for _, item := range slice {
+		key := keyFn(item)
+		if _, exists := groups[key]; !exists {
+			order = append(order, key)
+		}
+		groups[key] = append(groups[key], item)
+	}
+
+	return order, groups
+}
