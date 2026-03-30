cd9fcc890db1798dfbd0671344a01c13165dccb1
Bartek Mucha <bartosz.mucha@arm.com>
2026-03-30 15:04:53 +0100

    chore: unnest tests
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/health/status_test.go
+++ internal/health/status_test.go
@@ -21,28 +21,26 @@ func (m *mockRunner) Run(cmd string) (string, error) {
 }
 
 func TestProbeHealthStatus(t *testing.T) {
-	t.Run("ProbeHealthStatus", func(t *testing.T) {
-		t.Run("finds remote CPUs", func(t *testing.T) {
-			r := new(mockRunner)
-			r.On("Run", command.WrapInLoginShell("ls /sys/class/remoteproc")).Return("remoteproc0\nremoteproc1", nil)
-			r.On("Run", command.WrapInLoginShell("cat /sys/class/remoteproc/*/name")).Return("foo\nbar", nil)
-			r.On("Run", mock.AnythingOfType("string")).Maybe().Return("", fmt.Errorf("not found"))
+	t.Run("finds remote CPUs", func(t *testing.T) {
+		r := new(mockRunner)
+		r.On("Run", command.WrapInLoginShell("ls /sys/class/remoteproc")).Return("remoteproc0\nremoteproc1", nil)
+		r.On("Run", command.WrapInLoginShell("cat /sys/class/remoteproc/*/name")).Return("foo\nbar", nil)
+		r.On("Run", mock.AnythingOfType("string")).Maybe().Return("", fmt.Errorf("not found"))
 
-			ts := health.ProbeHealthStatus(r)
+		ts := health.ProbeHealthStatus(r)
 
-			want := health.HardwareProfile{RemoteCPU: []target.RemoteprocCPU{{Name: "foo"}, {Name: "bar"}}}
-			assert.Equal(t, want, ts.Hardware)
-			r.AssertExpectations(t)
-		})
+		want := health.HardwareProfile{RemoteCPU: []target.RemoteprocCPU{{Name: "foo"}, {Name: "bar"}}}
+		assert.Equal(t, want, ts.Hardware)
+		r.AssertExpectations(t)
+	})
 
-		t.Run("succeeds when no remoteproc support", func(t *testing.T) {
-			r := new(mockRunner)
-			r.On("Run", mock.AnythingOfType("string")).Return("", fmt.Errorf("no such directory"))
+	t.Run("succeeds when no remoteproc support", func(t *testing.T) {
+		r := new(mockRunner)
+		r.On("Run", mock.AnythingOfType("string")).Return("", fmt.Errorf("no such directory"))
 
-			ts := health.ProbeHealthStatus(r)
+		ts := health.ProbeHealthStatus(r)
 
-			assert.Len(t, ts.Hardware.RemoteCPU, 0)
-			r.AssertExpectations(t)
-		})
+		assert.Len(t, ts.Hardware.RemoteCPU, 0)
+		r.AssertExpectations(t)
 	})
 }
