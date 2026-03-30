4f20abb957749dd2f78bfef751a39120dc048058
Bartek Mucha <bartosz.mucha@arm.com>
2026-03-30 14:03:46 +0100

    refactor: use `testify/mock` in `ProbeHealthStatus` tests
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/health/status_test.go
+++ internal/health/status_test.go
@@ -2,50 +2,47 @@ package health_test
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
+	"github.com/arm/topo/internal/command"
 	"github.com/arm/topo/internal/health"
 	"github.com/arm/topo/internal/target"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-type fakeRunner struct {
-	run func(command string) (string, error)
+type mockRunner struct {
+	mock.Mock
 }
 
-func (f fakeRunner) Run(command string) (string, error) {
-	return f.run(command)
+func (m *mockRunner) Run(cmd string) (string, error) {
+	args := m.Called(cmd)
+	return args.String(0), args.Error(1)
 }
 
 func TestProbeHealthStatus(t *testing.T) {
 	t.Run("ProbeHealthStatus", func(t *testing.T) {
 		t.Run("finds remote CPUs", func(t *testing.T) {
-			r := fakeRunner{run: func(command string) (string, error) {
-				switch {
-				case strings.Contains(command, "ls /sys/class/remoteproc"):
-					return "remoteproc0\nremoteproc1", nil
-				case strings.Contains(command, "cat /sys/class/remoteproc"):
-					return "foo\nbar", nil
-				default:
-					return "", fmt.Errorf("unexpected command: %s", command)
-				}
-			}}
+			r := new(mockRunner)
+			r.On("Run", command.WrapInLoginShell("ls /sys/class/remoteproc")).Return("remoteproc0\nremoteproc1", nil)
+			r.On("Run", command.WrapInLoginShell("cat /sys/class/remoteproc/*/name")).Return("foo\nbar", nil)
+			r.On("Run", mock.AnythingOfType("string")).Maybe().Return("", fmt.Errorf("not found"))
 
 			ts := health.ProbeHealthStatus(r)
 
 			want := health.HardwareProfile{RemoteCPU: []target.RemoteprocCPU{{Name: "foo"}, {Name: "bar"}}}
 			assert.Equal(t, want, ts.Hardware)
+			r.AssertExpectations(t)
 		})
 
 		t.Run("succeeds when no remoteproc support", func(t *testing.T) {
-			r := fakeRunner{run: func(command string) (string, error) {
-				return "", fmt.Errorf("no such directory")
-			}}
+			r := new(mockRunner)
+			r.On("Run", mock.AnythingOfType("string")).Return("", fmt.Errorf("no such directory"))
 
 			ts := health.ProbeHealthStatus(r)
 
 			assert.Len(t, ts.Hardware.RemoteCPU, 0)
+			r.AssertExpectations(t)
 		})
 	})
 }
