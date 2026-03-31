3ce5212b019f47ea4590d5f20685166f2b737c3a
awphi <26072111+awphi@users.noreply.github.com>
2026-03-31 15:29:28 +0100

    fix JSON output
    Signed-off-by: awphi <26072111+awphi@users.noreply.github.com>

--- cmd/topo/main.go
+++ cmd/topo/main.go
@@ -7,9 +7,6 @@ import (
 )
 
 func main() {
-	outputFormat := resolveOutput(rootCmd)
-	logger.SetOptions(logger.Options{Format: outputFormat})
-
 	if err := rootCmd.Execute(); err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
