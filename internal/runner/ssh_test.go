396089ee3d8cd1c18419ce3939c84d3b189fbd35
Bartek Mucha <bartosz.mucha@arm.com>
2026-04-01 13:15:37 +0100

    Delete Connection and move SSHOptions tests to runner package
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/runner/ssh_test.go
+++ internal/runner/ssh_test.go
@@ -1,19 +1,19 @@
-package target_test
+package runner_test
 
 import (
 	"testing"
 	"time"
 
-	"github.com/arm/topo/internal/target"
+	"github.com/arm/topo/internal/runner"
 	"github.com/arm/topo/internal/testutil"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConnectionOptions(t *testing.T) {
+func TestSSHOptions(t *testing.T) {
 	t.Run("SSHArgs", func(t *testing.T) {
 		t.Run("with multiplexing enabled on non-windows includes Control args", func(t *testing.T) {
 			testutil.RequireOS(t, "linux")
-			opts := target.ConnectionOptions{Multiplex: true}
+			opts := runner.SSHOptions{Multiplex: true}
 
 			assert.Equal(t, []string{
 				"-o", "ControlMaster=auto",
@@ -24,27 +24,23 @@ func TestConnectionOptions(t *testing.T) {
 
 		t.Run("with multiplexing enabled on windows includes no Control args", func(t *testing.T) {
 			testutil.RequireOS(t, "windows")
-			opts := target.ConnectionOptions{Multiplex: true}
+			opts := runner.SSHOptions{Multiplex: true}
 
 			assert.Nil(t, opts.SSHArgs())
 		})
 
 		t.Run("with multiplexing disabled includes no Control args", func(t *testing.T) {
-			opts := target.ConnectionOptions{Multiplex: false}
-
-			assert.Nil(t, opts.SSHArgs())
+			assert.Nil(t, runner.SSHOptions{Multiplex: false}.SSHArgs())
 		})
 
 		t.Run("with connect timeout includes ConnectTimeout arg", func(t *testing.T) {
-			opts := target.ConnectionOptions{ConnectTimeout: 10 * time.Second}
+			opts := runner.SSHOptions{ConnectTimeout: 10 * time.Second}
 
 			assert.Equal(t, []string{"-o", "ConnectTimeout=10"}, opts.SSHArgs())
 		})
 
 		t.Run("with no timeout includes no ConnectTimeout arg", func(t *testing.T) {
-			opts := target.ConnectionOptions{}
-
-			assert.Nil(t, opts.SSHArgs())
+			assert.Nil(t, runner.SSHOptions{}.SSHArgs())
 		})
 	})
 }
