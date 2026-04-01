b3dc2b7a547c02329e37577a693a11ba399202b8
Bartek Mucha <bartosz.mucha@arm.com>
2026-04-01 10:56:50 +0100

    Remove `ExecCmd`
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/ssh/exec.go
+++ internal/ssh/exec.go
@@ -7,25 +7,6 @@ import (
 	"slices"
 )
 
-// ExecCmd builds a command to be executed on the target host. If the target is localhost, it will run locally when executed.
-// Pass stdin data as optional parameter, or nil for no stdin.
-func ExecCmd(target Destination, command string, stdin []byte, sshArgs ...string) *exec.Cmd {
-	var cmd *exec.Cmd
-	if target.IsPlainLocalhost() {
-		// #nosec G204 -- command should be validated by callers
-		cmd = exec.Command("/bin/sh", "-c", command)
-	} else {
-		args := slices.Concat(sshArgs, []string{"--", target.String(), command})
-		// #nosec G204 -- command should be validated by callers
-		cmd = exec.Command("ssh", args...)
-	}
-
-	if stdin != nil {
-		cmd.Stdin = bytes.NewReader(stdin)
-	}
-	return cmd
-}
-
 // RunCommand executes a command on dest over SSH and returns stdout.
 // stderr is classified into a typed error when a known failure pattern is detected.
 // Pass stdin data as optional parameter, or nil for no stdin.
