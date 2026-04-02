36ea427b90814b359fc283462aa4b8ffed8cda53
Yejin Seo <yejseo01@arm.com>
2026-04-02 09:56:15 +0100

    use github action instead

--- scripts/release/check_commit.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def check_commit_message(title: str) -> bool:
-    pattern = re.compile(
-        r"^(revert: )?((feat|fix|docs|style|refactor|perf|test|build|ci|chore)(\([\w ._-]+\))?(!)?: .+)"
-    )
-    return bool(pattern.match(title))
