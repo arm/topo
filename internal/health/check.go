215fa5978c09343fddd7efc9230e014cb2cf6707
federicobozzini <federico.bozzini@gmail.com>
2026-04-20 16:21:31 +0100

    Show fix only if issue is actually detected
    Signed-off-by: federicobozzini <federico.bozzini@gmail.com>

--- internal/health/check.go
+++ internal/health/check.go
@@ -35,8 +35,14 @@ type BinaryExists struct {
 
 func (b BinaryExists) Run(ctx context.Context, r runner.Runner, dep Dependency) (string, error) {
 	err := r.BinaryExists(ctx, dep.Binary)
-	if b.Severity == SeverityWarning && err != nil && !errors.Is(err, runner.ErrTimeout) {
-		err = WarningError{Err: err}
+	if err == nil {
+		return b.Fix, nil
+	}
+	if errors.Is(err, runner.ErrTimeout) {
+		return "", err
+	}
+	if b.Severity == SeverityWarning {
+		return b.Fix, WarningError{Err: err}
 	}
 	return b.Fix, err
 }
