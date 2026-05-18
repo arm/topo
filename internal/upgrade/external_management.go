1c02cc0adc0aaa3039933c58036d1b4275335464
Bartek Mucha <bartosz.mucha@arm.com>
2026-05-18 13:44:59 +0100

    Simplify brew check
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/upgrade/external_management.go
+++ internal/upgrade/external_management.go
@@ -1,8 +1,8 @@
 package upgrade
 
 import (
-	"bytes"
 	"path/filepath"
+	"strings"
 )
 
 func IsTopoBinaryManagedExternally(binPath string) bool {
@@ -10,23 +10,6 @@ func IsTopoBinaryManagedExternally(binPath string) bool {
 }
 
 func isHomebrewManagedBinary(binPath string) bool {
-	parts := splitPath(binPath)
-	for i := 0; i < len(parts)-2; i++ {
-		if parts[i] == "Cellar" && parts[i+1] == "topo" {
-			return true
-		}
-	}
-	return false
-}
-
-func splitPath(path string) []string {
-	volume := filepath.VolumeName(path)
-	path = path[len(volume):]
-	parts := []string{}
-	for part := range bytes.SplitSeq([]byte(filepath.ToSlash(path)), []byte{'/'}) {
-		if len(part) > 0 {
-			parts = append(parts, string(part))
-		}
-	}
-	return parts
+	path := filepath.ToSlash(binPath)
+	return strings.Contains(path, "Cellar/topo/")
 }
