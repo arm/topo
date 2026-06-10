f6d5bac62836dc519dd835a2f6651d5aa6c82905
Bartek Mucha <bartosz.mucha@arm.com>
2026-06-10 11:47:24 +0100

    Use shas instead of `main`
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- scripts/update_templates/github_source.go
+++ scripts/update_templates/github_source.go
@@ -6,12 +6,11 @@ import (
 	"io"
 )
 
-// TODO: sha should become a hash
 var sourcesJSON = `[
-	{"repo": "Arm-Examples/topo-welcome", "sha": "main"},
-	{"repo": "Arm-Examples/topo-lightbulb-moment", "sha": "main"},
-	{"repo": "Arm-Examples/topo-cpu-ai-chat", "sha": "main"},
-	{"repo": "Arm-Examples/topo-simd-visual-benchmark", "sha": "main"}
+	{"repo": "Arm-Examples/topo-welcome", "sha": "8303e66db59a7a11e64877121f3db1b688d2011f"},
+	{"repo": "Arm-Examples/topo-lightbulb-moment", "sha": "c2b2e4a672cda67832372f77aeb1d1f71beee9a7"},
+	{"repo": "Arm-Examples/topo-cpu-ai-chat", "sha": "c06f343b9c753231714ac1cdbee7c7be5108e6b7"},
+	{"repo": "Arm-Examples/topo-simd-visual-benchmark", "sha": "f0cd31621ce79b4643df7e9bdd8eff26c20b338c"}
 ]`
 
 type GitHubSource struct {
