38767f26bf08d4deaa67732bb913b30cb623ca44
Bartek Mucha <bartosz.mucha@arm.com>
2026-07-23 11:37:21 +0100

    Improve high level readability
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/deploy/deploy.go
+++ internal/deploy/deploy.go
@@ -60,45 +60,46 @@ func Deploy(ctx context.Context, output io.Writer, composeFile string, opts Depl
 	}
 
 	targetHost := command.NewHostFromDestination(opts.TargetHost)
-	if !opts.TargetHost.IsPlainLocalhost() && opts.Registry == nil {
-		if err := runStep(output, "Transfer images", func() error {
-			return docker.TransferImagesViaPipe(ctx, output, composeFile, sourceHost, targetHost)
-		}); err != nil {
-			return err
-		}
-	}
-	if !opts.TargetHost.IsPlainLocalhost() && opts.Registry != nil {
-		if err := runStep(output, "Run registry", func() error {
-			return docker.EnsureRegistryRunning(ctx, output, DefaultRegistryContainerName, opts.Registry.Port)
-		}); err != nil {
-			return err
-		}
-		var tunnel *ssh.SSHTunnel
-		if err := runStep(output, "Open registry SSH tunnel", func() error {
-			var err error
-			tunnel, err = ssh.OpenSSHTunnel(ctx, output, opts.TargetHost, opts.Registry.Port, opts.Registry.UseControlSockets)
-			if err != nil {
-				return fmt.Errorf("failed to open SSH tunnel: %w; ensure port %s is free or specify a different one with `--registry-port`", err, opts.Registry.Port)
-			}
-			return nil
-		}); err != nil {
-			return err
-		}
-		defer func() {
-			deploymentError = errors.Join(deploymentError, closeTunnel(output, tunnel))
-		}()
-
-		if !opts.TargetHost.IsLocalhost() && !opts.Registry.SkipRemotePortCheck {
-			if err := runStep(output, "Check registry tunnel is not exposed on remote network", func() error {
-				return docker.CheckTunnelExposure(ctx, output, opts.TargetHost, opts.Registry.Port)
+	if !opts.TargetHost.IsPlainLocalhost() {
+		if opts.Registry == nil {
+			if err := runStep(output, "Transfer images", func() error {
+				return docker.TransferImagesViaPipe(ctx, output, composeFile, sourceHost, targetHost)
 			}); err != nil {
 				return err
 			}
-		}
-		if err := runStep(output, "Transfer via registry", func() error {
-			return docker.TransferImagesViaRegistry(ctx, output, composeFile, sourceHost, targetHost, opts.Registry.Port)
-		}); err != nil {
-			return err
+		} else {
+			if err := runStep(output, "Run registry", func() error {
+				return docker.EnsureRegistryRunning(ctx, output, DefaultRegistryContainerName, opts.Registry.Port)
+			}); err != nil {
+				return err
+			}
+			var tunnel *ssh.SSHTunnel
+			if err := runStep(output, "Open registry SSH tunnel", func() error {
+				var err error
+				tunnel, err = ssh.OpenSSHTunnel(ctx, output, opts.TargetHost, opts.Registry.Port, opts.Registry.UseControlSockets)
+				if err != nil {
+					return fmt.Errorf("failed to open SSH tunnel: %w; ensure port %s is free or specify a different one with `--registry-port`", err, opts.Registry.Port)
+				}
+				return nil
+			}); err != nil {
+				return err
+			}
+			defer func() {
+				deploymentError = errors.Join(deploymentError, closeTunnel(output, tunnel))
+			}()
+
+			if !opts.TargetHost.IsLocalhost() && !opts.Registry.SkipRemotePortCheck {
+				if err := runStep(output, "Check registry tunnel is not exposed on remote network", func() error {
+					return docker.CheckTunnelExposure(ctx, output, opts.TargetHost, opts.Registry.Port)
+				}); err != nil {
+					return err
+				}
+			}
+			if err := runStep(output, "Transfer via registry", func() error {
+				return docker.TransferImagesViaRegistry(ctx, output, composeFile, sourceHost, targetHost, opts.Registry.Port)
+			}); err != nil {
+				return err
+			}
 		}
 	}
 
