f0baec13c096c0b300572e2a6e205a670c1feb20
Bartek Mucha <bartosz.mucha@arm.com>
2026-09-04 13:33:55 +0100

    Ensure socket tunnel header is printed
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/deploy/podman/stop.go
+++ internal/deploy/podman/stop.go
@@ -10,23 +10,25 @@ import (
 )
 
 func Stop(ctx context.Context, output io.Writer, composeFile string, target ssh.Destination) (stopErr error) {
+	socket := LocalSocket
+	if !target.IsPlainLocalhost() {
+		if err := term.PrintHeader(output, "Open Podman socket SSH tunnel"); err != nil {
+			return err
+		}
+
+		tunnel, err := TunnelRemoteSocketPath(ctx, output, target)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			stopErr = errors.Join(stopErr, closeRemoteTunnel(tunnel))
+		}()
+
+		socket = NewSocket(tunnel.SocketURL())
+	}
+
 	if err := term.PrintHeader(output, "Stop services"); err != nil {
 		return err
 	}
-
-	socket := LocalSocket
-	if target.IsPlainLocalhost() {
-		return RunComposeCommand(ctx, output, socket, composeFile, "stop")
-	}
-
-	tunnel, err := TunnelRemoteSocketPath(ctx, output, target)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		stopErr = errors.Join(stopErr, closeRemoteTunnel(tunnel))
-	}()
-
-	socket = NewSocket(tunnel.SocketURL())
 	return RunComposeCommand(ctx, output, socket, composeFile, "stop")
 }
