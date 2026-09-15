ec9a7fee35e085a605c20a02529bead6631eaeb5
awphi <26072111+awphi@users.noreply.github.com>
2026-09-15 15:07:45 +0100

    remove semicolon
    Signed-off-by: awphi <26072111+awphi@users.noreply.github.com>

--- cmd/topo/root.go
+++ cmd/topo/root.go
@@ -186,7 +186,7 @@ const envFileFlag = "env-file"
 func addEnvFileFlag(cmd *cobra.Command) {
 	cmd.Flags().StringArray(
 		envFileFlag, []string{".env", env.DefaultFilename},
-		"path(s) to an env file to source values for compose interpolation; can be specified multiple times",
+		"path to env file to source values for compose interpolation",
 	)
 }
 
