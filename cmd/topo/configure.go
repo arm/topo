85642ec11f2ff996d067436dab58d60a6907b35c
adamwph <26072111+awphi@users.noreply.github.com>
2026-09-17 11:26:24 +0100

    feat: experimental env-file migration (#465)
    Signed-off-by: awphi <26072111+awphi@users.noreply.github.com>

--- cmd/topo/configure.go
+++ cmd/topo/configure.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/arm/topo/internal/env"
+	"github.com/arm/topo/internal/output/logger"
 	"github.com/arm/topo/internal/output/term"
 	"github.com/arm/topo/internal/parameter"
 	"github.com/arm/topo/internal/project"
 	"github.com/spf13/cobra"
 )
+
+var migrateToEnv bool
 
 var configureCmd = &cobra.Command{
 	Use:   "configure [PARAMETER=VALUE ...]",
@@ -32,6 +37,15 @@ interactive prompts.`,
 			return err
 		}
 
+		if migrateToEnv {
+			err := project.MigrateToEnv(composeFile)
+			if err != nil {
+				return err
+			}
+			logger.Info(fmt.Sprintf("successfully migrated %q to be parameterized from %q", composeFile, env.DefaultFilename))
+			return nil
+		}
+
 		var resolvers []parameter.Resolver
 		if len(args) > 0 {
 			cliResolver, err := parameter.NewCLIResolver(args)
@@ -52,5 +66,8 @@ interactive prompts.`,
 
 func init() {
 	addComposeFileFlag(configureCmd)
+	if experimentalFeaturesEnabled() {
+		configureCmd.Flags().BoolVar(&migrateToEnv, "migrate-to-env", false, fmt.Sprintf("move parameter values from the compose file to %q, updating the compose file accordingly", env.DefaultFilename))
+	}
 	rootCmd.AddCommand(configureCmd)
 }
