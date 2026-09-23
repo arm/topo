962dacc22a3eeeed01119e52753846e35531be51
Bartek Mucha <bartosz.mucha@arm.com>
2026-09-23 16:05:40 +0100

    feat!: drop `--accept-new-host-keys` from `topo health` (#503)
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- internal/probe/ssh_authentication.go
+++ internal/probe/ssh_authentication.go
@@ -7,22 +7,13 @@ import (
 )
 
 // SSHAuthentication verifies SSH connectivity by attempting public key authentication.
-func SSHAuthentication(ctx context.Context, r *runner.SSH, acceptNewHostKeys bool) error {
-	_, _, err := r.RunWithArgs(ctx, "true", sshAuthArgs(acceptNewHostKeys)...)
-	return err
-}
-
-func sshAuthArgs(acceptNewHostKeys bool) []string {
-	args := []string{
+func SSHAuthentication(ctx context.Context, r *runner.SSH) error {
+	_, _, err := r.RunWithArgs(ctx, "true",
 		"-o", "BatchMode=yes",
 		"-o", "PreferredAuthentications=publickey",
 		"-o", "PasswordAuthentication=no",
 		"-o", "NumberOfPasswordPrompts=0",
-	}
-	if acceptNewHostKeys {
-		args = append(args, "-o", "StrictHostKeyChecking=accept-new")
-	} else {
-		args = append(args, "-o", "StrictHostKeyChecking=yes")
-	}
-	return args
+		"-o", "StrictHostKeyChecking=yes",
+	)
+	return err
 }
