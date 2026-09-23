7a90a67abac6762ea61150a65ec2a72ac44e6413
adamwph <26072111+awphi@users.noreply.github.com>
2026-09-23 16:25:51 +0100

    feat: remove `topo migrate-ssh` (#500)
    Signed-off-by: awphi <26072111+awphi@users.noreply.github.com>

--- cmd/topo/setup_keys.go
+++ cmd/topo/setup_keys.go
@@ -34,7 +34,7 @@ var setupKeysCmd = &cobra.Command{
 		if isLegacyDir, err := ssh.IsLegacyTopoConfigDirectory(sshDir); err != nil {
 			return err
 		} else if isLegacyDir {
-			return fmt.Errorf("legacy topo ssh config entries found; run 'topo migrate-ssh' to migrate to the new single-file format")
+			return fmt.Errorf("legacy topo ssh config entries found; downgrade to Topo 13.0.0 (https://github.com/arm/topo/blob/v13.0.0/docs/introduction/install.mdx#install-a-specific-version-or-use-a-custom-directory) or below and run 'topo migrate-ssh' to migrate to the new single-file format")
 		}
 
 		dest := ssh.NewDestination(targetArg)
